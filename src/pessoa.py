4b53a73f2f097c970ff7260a083d28575bb6255b
Fabricio J Barth <fabricio.barth@gmail.com>
2022-05-05 18:48:34 -0300

    pessoa sem futebol

--- src/pessoa.py
+++ src/pessoa.py
@@ -1,15 +1,11 @@
 class Pessoa:
 
-    def __init__(self, nome, idade, gosta_futebol):
+    def __init__(self, nome, idade):
         self.nome = nome
         self.idade = idade
-        self.gosta_futebol = gosta_futebol
 
     def mensagem(self):
         return f'Olá! Meu nome é {self.nome} e eu tenho {self.idade} anos.'
 
-    def gosta_futebol(self):
-        if self.gosta_futebol:
-            return "Sim! Eu gosto de futebol!"
-        return "Não. Não gosto de futebol"
+
 
