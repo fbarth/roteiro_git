03dbf8ecbd078c2bbc9204441d9fb9ae497fd563
fabriciojb <105012667+fabriciojb@users.noreply.github.com>
2022-05-05 19:26:59 -0300

    nova interacao (#1)

--- src/programa.py
+++ src/programa.py
@@ -2,15 +2,22 @@ from pessoa import Pessoa
 from gato import Gato
 from galinha import Galinha
 
-p1 = Pessoa('João', 42)
-p2 = Pessoa('Maria', 18)
+qtd_pessoas = int(input('Quantas pessoas devem ser cadastradas?'))
+pessoas = []
+
+for i in range(qtd_pessoas): 
+    nome = input('Nome: ')
+    idade = int(input('Idade: '))
+    p = Pessoa(nome, idade)
+    pessoas.append(p)
 
 g1 = Gato('Bola', 1)
 
+for i in range(qtd_pessoas): 
+    print(pessoas[i].mesagem())
 
-print(p1.mensagem())
-print(p2.mensagem())
 print(g1.mensagem())
 
 ga1 = Galinha()
 print(ga1.mensagem())
+
