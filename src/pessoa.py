2a3fe37e0389be75b93c6c022c53c1bd3804c538
Fabricio J Barth <fabricio.barth@gmail.com>
2022-05-05 14:51:10 -0300

    comecando com o codigo

---
+++ src/pessoa.py
@@ -0,0 +1,8 @@
+class Pessoa:
+
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+
+    def mensagem(self):
+        return f'Olá! Meu nome é {self.nome} e eu tenho {self.idade} anos.'
---
+++ src/programa.py
@@ -0,0 +1,12 @@
+from pessoa import Pessoa
+from gato import Gato
+
+p1 = Pessoa('João', 42)
+p2 = Pessoa('Maria', 18)
+
+g1 = Gato('Bola', 1)
+
+
+print(p1.mensagem())
+print(p2.mensagem())
+print(g1.mensagem())
